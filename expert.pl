9eef8e4d1b3512e2fcff04ccf54e4a369d8a7c24
ZH1896ZH <64612119+ZH1896ZH@users.noreply.github.com>
2020-10-06 17:44:45 +0200

    Update expert.pl
    added risk values, doesnt work so far :-(

--- expert.pl
+++ expert.pl
@@ -17,6 +17,7 @@ person(bob).   % not swiss
 person(mike).  % hearing defect
 person(hans).  % mental illness and over 65
 person(peter).
+person(fredy). % mental illness and over 55
 
 
 age(john, 55).
@@ -26,6 +27,7 @@ age(bob, 45).
 age(mike, 25).
 age(hans, 68).
 age(peter, 68).
+age(fredy, 57).
 
 
 citizen(john, ch).
@@ -35,6 +37,7 @@ citizen(bob, us).
 citizen(mike, ch).
 citizen(hans, ch).
 citizen(peter, ch).
+citizen(fredy, ch).
 
 
 patient(mike, hearing).
@@ -43,14 +46,14 @@ patient(hans, mental).
 
 %------------------------------------------------------------------------------
 % The following rules are phrased in a way, where they return true, if the
-% condition allows them to be elligible.
+% condition allows them to be eligible.
 % That way to be eligibe all rules for that person must return true.
 %------------------------------------------------------------------------------
 
-% no people younger than 21 are eligible
+% people younger than 21 are not eligible
 old_enough(X) :- person(X), age(X,Y), Y > 21.
 
-% people over 70 years of age not eligible
+% people older than 70 are not eligible
 young_enough(X) :- person(X), age(X,Y), Y < 70.
 
 % people living in switzerland are eligible
@@ -59,10 +62,22 @@ swiss(X) :- person(X), citizen(X,Y), Y = ch.
 % people with hearing defects are not eligble
 good_hearing(X) :- person(X), \+ patient(X, hearing).
 
-% is person over 65 and suffers from mental ilnesses?
+% people older than 65 and suffer from mental ilnesses are not eligble
 mental_check(X) :- person(X), \+ (age(X, Y), Y > 64, patient(X, mental)).
 
+% people having three or more diseases
+high_risk_value(X):- person(X), \+ diseases_count >= 3.
+
+% people older than 55 AND (suffer from mental ilnesses OR having 2 diseases)
+medium_risk_value(X):- person(X), \+ (age(X,Y), Y > 55, patient(X, mental)).
+
+% people whose risk values are not high and medium
+low_risk_value(X):- person(X), \+ (medium_risk_value(X);  high_risk_value(X)).
+
 % is person actually eilgible?
-eligible(X) :- old_enough(X), young_enough(X), swiss(X), good_hearing(X), mental_check(X).
+eligible(X) :- old_enough(X), young_enough(X), swiss(X), good_hearing(X), 
+    mental_check(X), low_risk_value(X).
 
-
+% is person actually eilgible, but has to pay extra fees?
+eligible_with_extra_fee(X) :- old_enough(X), young_enough(X), swiss(X), good_hearing(X), 
+    mental_check(X), medium_risk_value(X).
